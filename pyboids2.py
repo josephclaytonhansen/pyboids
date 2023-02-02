f2b7f8117af90c306928dc6d6b14e3f6dcfcec7e
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-02-02 14:55:24 -0600

    Update pyboids2.py

--- pyboids2.py
+++ pyboids2.py
@@ -48,6 +48,7 @@ class GlobalParameters: #seed, count
         self.air_speed_variation = .5 #expose this!
         self.air_speed = .8 #expose this!
         self.personal_space_multiplier = .09 #expose this!
+        self.underwater = False #expose this!
 
 class Critter: #wrapper for Blender object with some additional information 
     def __init__(self, name, obj):
@@ -61,6 +62,12 @@ class Critter: #wrapper for Blender object with some additional information
         self.neighbors = []
         self.lneighbors = 0
         self.initialized = False
+        
+        self.air_time = 0
+        self.energy = 200
+        self.landing_time = 0
+        self.recharge_time = 40
+        self.is_flying = True
     
     def __str__(self):
         return self.name+"  Neighbors: " + str((self.lneighbors))
@@ -108,6 +115,13 @@ def fillCollectionWithCritters(critter, col, count):
         r.initialized = True
         r.personal_space = 1120 * g.personal_space_multiplier
         r.perception_length = r.personal_space - 2
+        j = r.air_speed / ((1/bpy.data.scenes["Scene"].pscalesf) * 100)
+        
+        self.recharge_time = int(k*20)+20
+        
+        if bpy.data.scenes["Scene"].pscale == True:
+            for d in [0,1,2]:
+                o.scale[d] = o.scale[d] + k
         
     if initialSpacing(count):
         finalizeInitialSpacing()
@@ -151,6 +165,7 @@ def syncWeights(critter, s, c, a, sw, cw, aw, mw):
     return working_velocity.normalized()
 
 def bakeFrameAndAdvance(scene):
+    g.underwater = bpy.data.scenes["Scene"].underwater
     #starting on g.sim_start, 
     if not g.baked and g.started:
         for critter in ClassyCritters:
@@ -230,6 +245,7 @@ class BoidsPanel(bpy.types.Panel):
     def draw(self, context):
         layout = self.layout
         scene = context.scene
+        layout.label(text="Static (initial) settings")
         row = layout.row()
         if not g.started:
             row.operator("pyboids.init")
@@ -237,14 +253,38 @@ class BoidsPanel(bpy.types.Panel):
             row.prop(scene, "boid_count")
             row = layout.row()
             row.prop(scene, "pscale")
+            row.prop(scene, "pscalesf")
         else:
             row.operator("pyboids.clear")
+        layout.separator()
+        layout.label(text="Dynamic settings")
+        row=layout.row()
+        row.prop(scene, "underwater")
         row = layout.row()
         row.prop(scene, "psm")
-        row = layout.row()
+        row = layout.row(align=True)
         row.prop(scene, "bas")
-        row = layout.row()
         row.prop(scene, "asv")
+        layout.separator()
+        if not bpy.data.scenes["Scene"].underwater:
+            layout.label(text="Landing settings")
+            layout.label(text="Recharge time:")
+            row = layout.row(align=True)
+            row.prop(scene, "min_rechargetime")
+            row.prop(scene, "max_rechargetime")
+            row = layout.row(align=True)
+            row.prop(scene, "min_airtime")
+            row.prop(scene, "max_airtime")
+            layout.separator()
+            row = layout.row(align=True)
+            row.prop(scene, "crawl")
+            row.prop(scene, "sticky")
+            row = layout.row(align=True)
+            row.prop(scene, "hopandfeed")
+            sub = row.row()
+            sub.scale_x = 1.5
+            sub.prop(scene, "hopsurface")
+        
         
 
 #----------------------------Operators------------------------
@@ -312,23 +352,54 @@ bpy.app.handlers.frame_change_post.append(bakeFrameAndAdvance)
 #---------------------------Register--------------------------
 
 def register():
+    b = bpy.types.Scene
     bpy.utils.register_class(BoidsPanel)
     bpy.utils.register_class(CreateBoids)
     bpy.utils.register_class(ResetBoids)
-    bpy.types.Scene.boid_count = IntProperty(name = "Boid count", default = 100)
-    bpy.types.Scene.bas = IntProperty(name = "Air speed", default = 8) #/10
-    bpy.types.Scene.asv = FloatProperty(name = "Air speed variation", default = .5)
-    bpy.types.Scene.psm = IntProperty(name = "Personal space multiplier", default = 9) #/100
-    bpy.types.Scene.pscale = BoolProperty(name = "Proportional size",
+    b.boid_count = IntProperty(name = "Boid count", default = 100)
+    b.bas = IntProperty(name = "Air speed", default = 8) #/10
+    b.asv = FloatProperty(name = "Variation", description = "Air speed variation", default = .5)
+    b.psm = IntProperty(name = "Personal space multiplier", default = 9) #/100
+    b.pscale = BoolProperty(name = "Size",
     description = "Boids scale relative to air speed", default = False)
+    b.pscalesf = IntProperty(name="Variation", default = 2) # *1/n * 100
+    b.underwater = BoolProperty(name = "Underwater (Disable Landing)",
+    description = "Underwater boid simulations don't land.\nThey remain in motion indefinitely")
+    b.min_rechargetime = IntProperty(name="Min",
+    default=20,
+    description="When a boid has been in the air long enough to run out of energy,\n it must land and recharge. This is the minimum amount of time (in frames) for recharge")
+    b.max_rechargetime = IntProperty(name="Max",
+    default=40,
+    description="When a boid has been in the air long enough to run out of energy,\n it must land and recharge. This is the maximum amount of time (in frames) for recharge")
+    b.min_airtime = IntProperty(name="Min air time",
+    default = 200,
+    description="How long (at least) a boid stays in the air before needing to land, in frames.")
+    b.max_airtime = IntProperty(name="Max air time",
+    default = 300,
+    description="How long (at most) a boid stays in the air before needing to land, in frames.\nSet to 0 to disable flying")
+    b.crawl = BoolProperty(name = "Crawl",
+    description = "If set to True, landed boids will crawl along the surfaces of set objects while landed, like insects.\nIf set to False, boids will be stationary while landed, like birds in a tree.")
+    b.hopandfeed=BoolProperty(name="Hop",
+    description = "If True, landed boids will hop on the selected surface like birds feeding.\nWithout a set surface, this will do nothing.")
+    b.hopsurface = PointerProperty(name="Surface", type = bpy.types.Object,
+    description="Set the surface for boids to hop around on")
+    b.sticky = BoolProperty(name="Sticky crawling", 
+    description = "If True, boids will crawl and ignore gravity, like insects.\nIf false, boids will only land on and crawl on faces facing upwards, like birds.")
+    
+    
+    
 
 
 def unregister():
     bpy.utils.unregister_class(BoidsPanel)
     bpy.utils.unregister_class(CreateBoids)
     bpy.utils.unregister_class(ResetBoids)
-    for i in [bpy.types.Scene.boid_count, bpy.types.Scene.bas,
-    bpy.types.Scene.asv, bpy.types.Scene.psm, bpy.types.Scene.pscale]:
+    for i in [b.boid_count, b.bas,
+    b.asv, b.psm, b.pscale,
+    b.pscalesf, b.underwater,
+    b.min_rechargetime, b.max_rechargetime,
+    b.min_airtime, b.max_airtime, b.sticky,
+    b.crawl, b.hopandfeed, b.hopsurface]:
         del i
 
 
